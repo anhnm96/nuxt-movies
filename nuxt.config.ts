ad1fb19cfd7acf79fce73c40c90a56c1318d6801
Nguyen Minh Anh <xiaosasori@gmail.com>
2023-08-21 17:56:08 +0900

    chore: add vueuse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/i18n'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/i18n', '@vueuse/nuxt'],
   i18n: {
     detectBrowserLanguage: {
       useCookie: true,
