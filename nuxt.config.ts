774e437806d42e3e436f240372fc72c5280331e0
Nguyen Minh Anh <xiaosasori@gmail.com>
2023-08-21 17:51:20 +0900

    chore: config i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
-});
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/i18n'],
+  i18n: {
+    detectBrowserLanguage: {
+      useCookie: true,
+      fallbackLocale: 'en',
+    },
+    locales: [
+      {
+        code: 'en',
+        name: 'English',
+        file: 'en.json',
+      },
+    ],
+    strategy: 'no_prefix',
+    lazy: true,
+    langDir: 'i18n',
+    defaultLocale: 'en',
+  },
+})
