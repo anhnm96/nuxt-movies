b713dfdfafbbb81ac740483eb985ce432e645f5e
Nguyen Minh Anh <xiaosasori@gmail.com>
2023-09-04 13:22:45 +0900

    feat: nav component

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>
+  <div
+    class="scrollbar grid grid-rows-[1fr_max-content] lg:grid-cols-[max-content_1fr]"
+  >
+    <TheNavbar />
     <NuxtPage />
   </div>
 </template>
---
+++ components/TheNavbar.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const navLinks = [
+  { name: 'Home', icon: 'i-ph-house', url: '/' },
+  { name: 'Movies', icon: 'i-ph-film-strip', url: '/movie' },
+  { name: 'TV Shows', icon: 'i-ph-television-simple', url: '/tv' },
+  { name: 'Search', icon: 'i-ph-magnifying-glass', url: '/search' },
+]
+</script>
+
+<template>
+  <nav
+    class="sticky bottom-0 z-20 row-start-2 bg-black lg:top-0 lg:row-start-auto lg:h-screen lg:border-r lg:border-gray-800"
+  >
+    <ul class="flex h-full lg:flex-col lg:justify-evenly">
+      <li v-for="link in navLinks" :key="link.name" class="w-full">
+        <NuxtLink
+          v-slot="{ isActive }"
+          :to="link.url"
+          class="flex justify-center py-2 text-3xl lg:p-5"
+        >
+          <Icon
+            :name="isActive ? `${link.icon}-fill` : link.icon"
+            :class="[isActive && 'text-primary']"
+          />
+        </NuxtLink>
+      </li>
+    </ul>
+  </nav>
+</template>
