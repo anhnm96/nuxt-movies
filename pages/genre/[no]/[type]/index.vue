2ce055dcf15f7acdd2e420b122860fed52f62dde
Nguyen Minh Anh <xiaosasori@gmail.com>
2023-10-09 15:18:33 +0900

    feat(query): category page

--- pages/genre/[no]/[type]/index.vue
+++ pages/genre/[no]/[type]/index.vue
@@ -1,26 +1,34 @@
 <script setup lang="ts">
-import type { Media, MediaType } from '@/types'
+import { useInfiniteQuery } from '@tanstack/vue-query'
+import type { MediaType } from '@/types'
+import { userGenreList } from '@/stores/media'
 
 const route = useRoute()
 const type = (route.params.type as MediaType) || 'movie'
 const no = route.params.no as string
 
-const items = reactive<Media[]>([])
+const { data: list, suspense: suspenseGenreList } = userGenreList(type)
+await suspenseGenreList()
+const name = list.value!.find((item) => item.id === +no)?.name
 
-const list = await getGenreList(type)
-const name = list.find((item) => item.id === +no)?.name
-
-let page = 1
-async function fetch() {
-  const { data } = await getMediaByGenre(type, no, page)
-  items.push(...data.value.results)
-  page++
+function fetch({ pageParam = 1 }) {
+  return getMediaByGenre(type, no, pageParam)
 }
-await fetch()
+const { data, fetchNextPage, isFetchingNextPage, hasNextPage, suspense } =
+  useInfiniteQuery({
+    queryKey: [type, 'genre', no],
+    queryFn: fetch,
+    getNextPageParam: (lastPage) => {
+      return lastPage.page < lastPage.total_pages
+        ? lastPage.page + 1
+        : undefined
+    },
+  })
 const title = `${type === 'movie' ? 'Movie' : 'TV'} Genre: ${name}`
 useHead({
   title,
 })
+if (process.server) await suspense()
 </script>
 
 <template>
@@ -29,15 +37,28 @@ useHead({
       {{ title }}
     </h2>
     <div
+      v-if="data"
       class="mt-4 grid grid-cols-[repeat(auto-fit,_minmax(208px,_1fr))] gap-2 xl:mt-5"
     >
-      <MediaCard
-        v-for="item in items"
-        :key="item.id"
-        :item="item"
-        :type="type"
+      <template v-for="group in data.pages" :key="group.page">
+        <MediaCard
+          v-for="item in group.results"
+          :key="item.id"
+          :item="item"
+          :type="type"
+        />
+      </template>
+    </div>
+    <div
+      v-if="isFetchingNextPage"
+      aria-label="loading"
+      class="mt-2 animate-pulse text-center"
+    >
+      <Icon
+        name="mingcute:loading-line"
+        class="animate-spin text-4xl text-primary"
       />
     </div>
-    <InfiniteLoad @infinite-load="fetch" />
+    <InfiniteLoad v-else-if="hasNextPage" @infinite-load="fetchNextPage" />
   </main>
 </template>
